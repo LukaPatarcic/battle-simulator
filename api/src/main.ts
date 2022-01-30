3781b5b6b83affc81e6ce437b657479e02c276eb
LukaPatarcic <luka.patarcic@auxality.com>
2022-01-30 22:53:45 +0100

    added swagger docs

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,14 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	app.enableCors({
-		allowedHeaders: '*',
-		origin: '*',
-		methods: ['POST', 'GET'],
-	});
-	app.enableShutdownHooks();
-	await app.listen(5000);
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+    methods: ['POST', 'GET'],
+  });
+  const config = new DocumentBuilder()
+    .setTitle('Battle Simulator example')
+    .setVersion('1.0')
+    .addTag('battle-simulator')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.enableShutdownHooks();
+  await app.listen(5000);
 }
 bootstrap();
